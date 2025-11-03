c79e74968001b37c022b8fe7cbc255c44a099f3d
Gia Duc <giaduc19042004@gmail.com>
2025-11-03 10:12:53 +0700

    update role for all route

--- src/routes/v1/admin/user.routes.ts
+++ src/routes/v1/admin/user.routes.ts
@@ -1,18 +1,21 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { userController } from '~/controllers/user.controller';
+import { authenticateTokenAdmin, authorizeRoles } from '~/middlewares/auth.middleware';
 import { userValidation } from '~/validations/user.validation';
 
 const Router = express.Router();
 
+Router.use(authenticateTokenAdmin);
+
 Router.route('/')
   .get(userController.getAllUser)
-  .post(userValidation.createNew, userController.createNew);
+  .post(authorizeRoles('admin', 'manager'), userValidation.createNew, userController.createNew);
 
 Router.route('/myInfo')
   .get(userController.getMyInfo);
 
 Router.route('/:id')
-  .get(userController.getUserDetail);
+  .get(authorizeRoles('admin', 'manager'), userController.getUserDetail);
 
 
 export const userRoute = Router;
